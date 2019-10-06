fa1060bb4313073e132f2353d726c250086ba60f
yaqootturman <yaqoot.turman@gmail.com>
2019-10-06 15:02:58 +0300

    file structure

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+// const router = require('./controllers/index.js')
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+
+app.use(express.json())
+app.use(express.urlencoded())
+// app.use(router)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+})
+
+module.exports = app
