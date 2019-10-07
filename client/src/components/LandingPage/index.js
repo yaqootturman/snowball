a095d4e22ba02e662a122999ee81326ecf96d95b
yaqootturman <yaqoot.turman@gmail.com>
2019-10-07 14:27:15 +0300

    display data of landing page

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './index.css'
+
+const LandingPage = () => {
+  return (
+    <div class='vertical'>
+      <h1 class='the-tiltle'>
+        Fight climate change together, every single day.
+      </h1>
+      <div class='circle1'></div>
+      <p>Learn what you can do</p>
+      <div class='circle2'></div>
+      <p>See what others are doing</p>
+      <div class='circle3'></div>
+      <p>Commit to a better future</p>
+      <h3>The Vision</h3>
+      <p>
+        A global network of people, fighting climate change together, through
+        our everyday actions.
+      </p>
+      <h3>How do we get there?</h3>
+      <p>
+        By showing people what they can do, giving them the tools to do it and
+        allowing them to commit to a better future. When we act together, small
+        changes have a big impact.
+      </p>
+      <h3>What’s the goal?</h3>
+      <p>
+        If we bring people together, the power lies with the people in the fight
+        against climate change. We want to create a movement to release that
+        inner power!
+      </p>
+      <h5>Let’s go!</h5>
+    </div>
+  )
+}
+export default LandingPage
