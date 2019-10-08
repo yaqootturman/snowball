ca277bd98b0f7326b3a051c11ab12a54fcdcc254
yaqootturman <yaqoot.turman@gmail.com>
2019-10-08 14:26:37 +0300

    className and media query

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -4,40 +4,40 @@ import './index.css'
 const LandingPage = () => {
   return (
     <>
-      <div class='vertical'>
-        <h1 class='vertical__tiltle'>
+      <div className='vertical'>
+        <h1 className='vertical__tiltle'>
           Fight climate change together, every single day.
         </h1>
-        <div class='vertical__contents'>
-          <div class='vertical__contents__circles'>
-            <div class='vertical__contents__circles__circle vertical__contents__circles__numberCircle1'>
+        <div className='vertical__contents'>
+          <div className='vertical__contents__circles'>
+            <div className='vertical__contents__circles__circle vertical__contents__circles__numberCircle1'>
               1
             </div>
-            <span class='vertical__contents__circles__span'>
+            <span className='vertical__contents__circles__span'>
               Learn what you can do
             </span>
           </div>
 
-          <div class='vertical__contents__circles'>
-            <div class='vertical__contents__circles__circle vertical__contents__circles__numberCircle2'>
+          <div className='vertical__contents__circles'>
+            <div className='vertical__contents__circles__circle vertical__contents__circles__numberCircle2'>
               2
             </div>
-            <span class='vertical__contents__circles__span'>
+            <span className='vertical__contents__circles__span'>
               See what others are doing
             </span>
           </div>
 
-          <div class='vertical__contents__circles'>
-            <div class='vertical__contents__circles__circle vertical__contents__circles__numberCircle3'>
+          <div className='vertical__contents__circles'>
+            <div className='vertical__contents__circles__circle vertical__contents__circles__numberCircle3'>
               3
             </div>
-            <span class='vertical__contents__circles__span'>
+            <span className='vertical__contents__circles__span'>
               Commit to a better future
             </span>
           </div>
         </div>
 
-        <div class='vertical__contents'>
+        <div className='vertical__contents'>
           <h3>The Vision</h3>
           <p>
             A global network of people, fighting climate change together,
@@ -45,7 +45,7 @@ const LandingPage = () => {
           </p>
         </div>
 
-        <div class='vertical__contents'>
+        <div className='vertical__contents'>
           <h3>How do we get there?</h3>
           <p>
             By showing people what they can do, giving them the tools to do it
@@ -54,7 +54,7 @@ const LandingPage = () => {
           </p>
         </div>
 
-        <div class='vertical__contents'>
+        <div className='vertical__contents'>
           <h3>What’s the goal?</h3>
           <p>
             If we bring people together, the power lies with the people in the
@@ -63,10 +63,10 @@ const LandingPage = () => {
           </p>
         </div>
       </div>
-      <div class='start'>
+      <div className='start'>
         <button
           onClick={console.log('this should push to home page')}
-          class='start__letsgo'
+          className='start__letsgo'
         >
           Let’s go!
         </button>
