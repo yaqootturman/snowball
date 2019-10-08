830a294dccac2eb07b51fd7b29481876fcff5a27
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-08 12:06:22 +0300

    put path to the category

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Category from "./components/Category"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/dashboard" component={Category} />
+      </Switch>
+    </React.Fragment>
+  </Router>
+)
 
-export default App;
+export default App
