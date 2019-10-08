9ea86071921101cdd786767da593150a6a0b770e
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-08 12:11:11 +0300

    put the controllers of get categories information in index

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const getCategoryInfo = require("./getCategoryInfo")
+
+router.get("/api/dashboard", getCategoryInfo.get)
+
+module.exports = router
