6c73600fd618b13b9d64f224b3ce0db3e3343ccd
Rand Inaim <rand.inaim95@gmail.com>
2019-10-09 14:57:00 +0300

    getting user pledges list from database is brought to the home page

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-
+const router = require('./controllers/index')
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
+app.use(router)
 app.use(express.json())
 app.use(express.urlencoded())
 
