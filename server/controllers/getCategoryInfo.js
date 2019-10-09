dd87198b9411a706e37d9ac2ff1a222cd5223f11
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-09 20:12:33 +0300

    removed console.log

--- server/controllers/getCategoryInfo.js
+++ server/controllers/getCategoryInfo.js
@@ -1,8 +1,7 @@
-const getCategoryInfo = require("./../database/queries/getCategory")
+const getCategoryInfo = require('./../database/queries/getCategory')
 
 exports.get = (req, res) => {
-  console.log("hi")
   getCategoryInfo()
     .then(data => res.json({ data }))
-    .catch(() => res.status(500).json({ err: "login Error" }))
+    .catch(() => res.status(500).json({ err: 'login Error' }))
 }
