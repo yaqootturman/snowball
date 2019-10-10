05bbe8ac35a1a4ab75d9ac2098e98bec67a78df1
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-10 10:18:31 +0300

    put the category component in dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Category from "./components/Category"
 import LandingPage from "./components/LandingPage"
+import DashboardPage from './components/DashboardPage'
+
 
 const App = () => (
   <Router>
     <React.Fragment>
       <Switch>
-        <Route exact path="/category" component={Category} />
+        <Route exact path="/dashboard" component={DashboardPage} />
         <Route exact path="/" component={LandingPage} />
       </Switch>
     </React.Fragment>
---
+++ client/src/components/DashboardPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Category from '../Category'
+
+
+const DashboardPage = props => {
+  return (
+    <>
+      <Category {...props} />
+    </>
+  )
+}
+export default DashboardPage
