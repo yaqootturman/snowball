a45a08052bb3e50ddbedf2235467a21a55787fd8
yaqootturman <yaqoot.turman@gmail.com>
2019-10-10 16:16:52 +0300

    to-continue

--- client/src/components/PledgItem/PledgeItem.js
+++ client/src/components/PledgItem/PledgeItem.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './style.css'
 const PledgeItem = element => {
   return (
-    <div className="pledge--div">
-      <img className="pledge--div__img" src={element.img} />
-      <p className="pledge--div__title">{element.title}</p>
-      <p className="pledge--div__description">{element.description}</p>
-      <span className="pledge--div__already">PLEDGES ALREADY </span>
-      <span className="pledge--div__number">{element.number_of_enrollement}</span>
+    <div className="pledge-div">
+      <img className="pledge-div__img" src={element.img} />
+      <p className="pledge-div__title">{element.title}</p>
+      <p className="pledge-div__description">{element.description}</p>
+      <span className="pledge-div__already">PLEDGES ALREADY </span>
+      <span className="pledge-div__number">{element.number_of_enrollement}</span>
     </div>
   )
 }
