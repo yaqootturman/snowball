77c950292f9b019f2edea8ae91b471273a31bcf1
Muniralsharif <munir.sniper@gmail.com>
2019-10-10 11:37:36 +0300

    delete files

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import LandingPage from './components/LandingPage'
-import PledgeConfirmPage from './components/PledgeConfirmPage'
 const App = () => {
 
   return (
