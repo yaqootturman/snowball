1d0965684e5ec3c1ed601a6b0a940a625d342be5
yaqootturman <yaqoot.turman@gmail.com>
2019-10-10 15:21:09 +0300

    styling complete

--- client/src/components/PledgItem/PledgeItem.js
+++ client/src/components/PledgItem/PledgeItem.js
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import './style.css'
 const PledgeItem = element => {
   return (
-    <>
-      <h1>{element.img}</h1>
-      <p>{element.title}</p>
-      <p>{element.description}</p>
-      <p>{element.further_info}</p>
-      <p>{element.number_of_enrollement}</p>
-      <p>{element.importance}</p>
-    </>
+    <div className="pledge--div">
+      <img className="pledge--div__img" src={element.img} />
+      <p className="pledge--div__title">{element.title}</p>
+      <p className="pledge--div__description">{element.description}</p>
+      <span className="pledge--div__already">PLEDGES ALREADY </span>
+      <span className="pledge--div__number">{element.number_of_enrollement}</span>
+    </div>
   )
 }
 export default PledgeItem
