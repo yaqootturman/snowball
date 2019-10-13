2ff80cc24c5845405b63a7007a40bfa122a33077
yaqootturman <yaqoot.turman@gmail.com>
2019-10-13 09:42:53 +0300

    add an alt prop for img tag

--- client/src/components/PledgItem/PledgeItem.js
+++ client/src/components/PledgItem/PledgeItem.js
@@ -4,7 +4,7 @@ const PledgeItem = props => {
   const { element, history } = props
   return (
     <div className="pledge-div">
-      <img onClick={() => { console.log("history.push('')") }} className="pledge-div__img" src={element.img} />
+      <img onClick={() => { console.log("history.push('')") }} alt={element.title} className="pledge-div__img" src={element.img} />
       <p className="pledge-div__title">{element.title}</p>
       <p className="pledge-div__description">{element.description}</p>
       <span className="pledge-div__already">PLEDGES ALREADY </span>
