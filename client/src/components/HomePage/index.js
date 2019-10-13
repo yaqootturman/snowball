41d75bd8a0150f93b8e68e626474975cae2592ec
Rand Inaim <rand.inaim95@gmail.com>
2019-10-13 11:07:14 +0300

    styling for one pledge

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import UserPledges from '../UserPledges'
 import axios from 'axios'
+import './style.css'
 
 class HomePage extends Component {
   state = {
@@ -20,8 +21,8 @@ class HomePage extends Component {
     console.log(pledges[0], 'one pledge')
     return (
       <React.Fragment>
-        <h1>My Pledges</h1>
-        <h5>TOTAL PLEDGES{pledges.length}</h5>
+        <h1 className="home-title">My Pledges</h1>
+        <h5 className="home-title__subtitle">TOTAL PLEDGES{pledges.length}</h5>
 
         {!pledges.length ? <h2>Loading...</h2> : <UserPledges userPledge={pledges[1]} />}
 
