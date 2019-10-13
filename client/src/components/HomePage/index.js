af4de4a01c9f351152c6608127a29a5562206143
Rand Inaim <rand.inaim95@gmail.com>
2019-10-13 11:45:50 +0300

    styling for user pledges page

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -11,23 +11,21 @@ class HomePage extends Component {
     const userId = 1
     axios.get(`/api/home/${userId}`).then(({ data }) => {
       this.setState({ pledges: data })
-      console.log(data, 'data')
     })
       .catch(error => console.log('axios error', error))
   }
   render() {
     const { pledges } = this.state
-    console.log(pledges, 'pledges list')
-    console.log(pledges[0], 'one pledge')
     return (
       <React.Fragment>
         <h1 className="home-title">My Pledges</h1>
-        <h5 className="home-title__subtitle">TOTAL PLEDGES{pledges.length}</h5>
+        <h5 className="home-title__subtitle">TOTAL PLEDGES: {pledges.length}</h5>
 
-        {!pledges.length ? <h2>Loading...</h2> : <UserPledges userPledge={pledges[1]} />}
-
-
-
+        {!pledges.length ? <h2>Loading...</h2> :
+          pledges.map(onePledge => {
+            return <UserPledges userPledge={onePledge} />
+          })
+        }
       </React.Fragment>
     )
   }
