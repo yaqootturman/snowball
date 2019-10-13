1c1506b42e27b2d9c24d87422e50e7a1014eb305
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-13 10:20:09 +0300

    make component to show number of pledges which user make it and all pledges

--- client/src/components/NumberOfPledges/index.js
+++ client/src/components/NumberOfPledges/index.js
@@ -5,6 +5,7 @@ import axios from "axios"
 class NumberOfPledges extends Component {
   state = {
     details: [],
+    allPledges: []
   }
   componentDidMount = () => {
     const userId = 1
@@ -14,19 +15,32 @@ class NumberOfPledges extends Component {
       this.setState(
         { details: data })
     })
+    axios.get('/api/dashboard_number_of_all_pledges').then(({ data }) => {
+      console.log("r2", data);
+
+      this.setState(
+        { allPledges: data })
+    })
 
   }
 
   render() {
     const { data } = this.state.details
+    const { data2 } = this.state.allPledges
+    console.log("2", data2);
+
+
     return (
       <div className="container">
         <h1 className="container__dashboard__title">Action Dashboard</h1>
+        {/* {!data2 ? <h1>loading</h1> : <p>{data2.length}</p>} */}
         {!data ? (
           <h1>Loading</h1>
         ) :
           (
-            <div className="container__numberOfPledges">{data.length}</div>
+            <div className="container__numberOfPledges">
+              {!data2 ? <h1>loading</h1> : (<div><p>{data2.length}/{data2.length}</p> <h3 className="container__pledges">PLEDGES</h3></div>)}
+            </div>
           )
         }
       </div >
