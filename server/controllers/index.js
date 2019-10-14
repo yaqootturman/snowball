d4285d1c27c353f0d5f4701bef0a94a3850743cb
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-14 10:50:03 +0300

    create controller to calculation

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const getCategoryInfo = require('./getCategoryInfo')
 const getNumberOfUserPledges = require('./getNumberOfUserPledges')
 const getAllPledges = require('./getAllPledges')
+const getPledgesInCategory = require('./getPledgesInCategory')
+const getNumberPledgesCategory = require('./getNumberPledgesCategory')
 
 router.get('/api/dashboard', getCategoryInfo.get)
 router.get('/api/dashboard_number_of_pledges/:userId', getNumberOfUserPledges.get)
 router.get('/api/dashboard_number_of_all_pledges', getAllPledges.get)
+router.get('/api/dasboard_pledges_in_category/:userId', getPledgesInCategory.get)
+router.get('/api/dashboard_pledges_category/:categoryName', getNumberPledgesCategory.get)
 
 module.exports = router
