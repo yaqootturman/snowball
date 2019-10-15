06f58a4acef9c4af9b70c5a4af1b07ece471bf19
Rand Inaim <rand.inaim95@gmail.com>
2019-10-15 15:43:47 +0300

    router middleware is reordered in app.js

--- server/app.js
+++ server/app.js
@@ -4,9 +4,10 @@ const app = express()
 const router = require('./controllers/index')
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
-app.use(router)
 app.use(express.json())
 app.use(express.urlencoded())
+
+app.use(router)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
