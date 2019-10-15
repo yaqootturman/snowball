a5cdd6a249fd66e9eab5d06047256c9a39460266
Rand Inaim <rand.inaim95@gmail.com>
2019-10-15 15:17:24 +0300

    naming in userpledge page are edited

--- client/src/components/UserPledges/index.js
+++ client/src/components/UserPledges/index.js
@@ -7,8 +7,8 @@ const UserPledge = ({ history, userPledge: { pledge_id, title, description, numb
       <img className="user-pledge__image" alt="pledge photo" src={img} onClick={() => { history.push(`/action-category/pledge/${title}`, { data: pledge_id }) }} />
       <h5 className="user-pledge__title">{title}</h5>
       <span className="user-pledge__description">{description}</span>
-      <div className="user-pledge__enrollements"><img className="user-pledge__enrollements__image" alt="people" src="https://imgur.com/qw15p7I.png" />
-        <p className="user-pledge__enrollements__text">{number_of_enrollement}</p></div>
+      <div className="user-pledge__enrollements"><img className="user-pledge__image" alt="people" src="https://imgur.com/qw15p7I.png" />
+        <p className="user-pledge__text">{number_of_enrollement}</p></div>
     </div>
   )
 }
