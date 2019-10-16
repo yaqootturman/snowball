d6de0ec5f26564da10627a202282f652ce578dbb
Rand Inaim <rand.inaim95@gmail.com>
2019-10-16 11:17:19 +0300

    deleting warning

--- client/src/components/UserPledges/index.js
+++ client/src/components/UserPledges/index.js
@@ -4,7 +4,7 @@ import './style.css'
 const UserPledge = ({ history, userPledge: { pledge_id, title, description, number_of_enrollement, img } }) => {
   return (
     <div className="user-pledge">
-      <img className="user-pledge__pledge-image" alt="pledge photo" src={img} onClick={() => { history.push(`/action-category/pledge/${title}`, { data: pledge_id }) }} />
+      <img className="user-pledge__pledge-image" alt="user pledge" src={img} onClick={() => { history.push(`/action-category/pledge/${title}`, { data: pledge_id }) }} />
       <h5 className="user-pledge__pledge-title">{title}</h5>
       <span className="user-pledge__pledge-description">{description}</span>
       <div className="user-pledge__pledge-enrollements-badge"><img className="user-pledge__pledge-enrollements-icon" alt="people" src="https://imgur.com/qw15p7I.png" />
