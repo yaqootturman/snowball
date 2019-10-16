c98a3e14cd40792990991c66abbbe21f9f43883a
yaqootturman <yaqoot.turman@gmail.com>
2019-10-16 11:07:21 +0300

    fixing

---
+++ client/build/precache-manifest.01a134b6f0d77718540398f6f973dc3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31662e1701067e04116014ab01bc872b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e6c0b912a2b30a0c6a",
+    "url": "/static/css/main.39094ddc.chunk.css"
+  },
+  {
+    "revision": "3f6e0235ef4ac9440e7b",
+    "url": "/static/js/2.f4d2b851.chunk.js"
+  },
+  {
+    "revision": "11e6c0b912a2b30a0c6a",
+    "url": "/static/js/main.d5cc3f80.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
