2660020722edba08883fb2fd7836740bec4de409
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-19 20:16:19 +0300

    rename the file

--- server/controllers/getCategoryPledges.js
+++
@@ -1,8 +0,0 @@
-const getCategoryPledges = require('./../database/queries/getCategoryPledges')
-
-exports.getCategoryPledges = (req, res) => {
-  const { categoryID } = req.params
-  getCategoryPledges(categoryID)
-    .then(data => res.json(data.rows))
-    .catch(() => res.status(500).json({ err: ' Error' }))
-}
