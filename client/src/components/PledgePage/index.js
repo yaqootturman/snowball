f2538f83c534a9ad756791741b6df2edc157f2cc
yaqootturman <yaqoot.turman@gmail.com>
2019-10-20 11:11:00 +0300

    delete and confirm button in pledge page

--- client/src/components/PledgePage/index.js
+++ client/src/components/PledgePage/index.js
@@ -4,19 +4,38 @@ import AppContext from '../../UserContext';
 import axios from 'axios'
 class PledgePage extends React.Component {
   static contextType = AppContext
+  addUserPledge() {
+    const { pledge_id } = this.props.location.state
+    const { history } = this.props
+    const userId = 1; //this should change to take id from props
 
+    history.push(`/${userId}/${pledge_id}/addPledge`)
+    this.setState(() => ({ pledgeExist: true }))
+  }
+
+  deleteUserPledge() {
+
+    const { pledge_id } = this.props.location.state
+
+    const userId = 1; //this should change to take id from props
+    axios.get(`/api/${userId}/${pledge_id}/deletePledge`).then(() => {
+
+      this.setState(() => ({ pledgeExist: false }))
+    }).catch(err => { console.log("axios err", err) })
+  }
   state = {
-    userPledges: [],
     userPledges: [],
     pledgeInfo: [],
     pledgeInstructions: [],
     pledgeProsCons: [],
     pledgeResources: [],
     pledgeReferences: [],
-    userExist: false
+    pledgeExist: false,
+    pledge_id: ''
   }
 
   componentDidMount() {
+    //to use context api in the function
     const userPledges = this.context
     this.setState({ userPledges })
 
@@ -24,8 +43,9 @@ class PledgePage extends React.Component {
     const { pledge_id } = this.props.location.state
     userPledges.map((element) => {
       if (element.pledge_id === pledge_id)
-        this.setState({ userExist: true })
+        this.setState({ pledgeExist: true, pledge_id })
     })
+
     axios.get(`/api/action-category/pledge/${pledge_id}`).then(Response => {
       const pledgeInfoPage = Response.data
       this.setState({
@@ -41,15 +61,18 @@ class PledgePage extends React.Component {
   }
 
   render() {
-    const { userPledges, pledgeInfo, pledgeInstructions, pledgeProsCons, pledgeResources, pledgeReferences, userExist } = this.state
+    const { userPledges, pledgeInfo, pledgeInstructions, pledgeProsCons, pledgeResources, pledgeReferences, pledgeExist } = this.state
 
     return (
       <>
-        {userExist ? <button>Cancel the pledge</button> : <buttton>Make the pledge</buttton>}        {
+        {
           !pledgeInfo.length ? <p>Loading...</p> :
             <>
               <div className="top-info">
                 <img className="top-info__img" alt="pledge information" src={pledgeInfo[0].img} />
+
+                {pledgeExist ? <button className="top-info__make-cancel" value={this.state.pledge_id} onClick={() => this.deleteUserPledge()}>Cancel the pledge</button> : <buttton className="top-info__make-cancel" value={this.state.pledge_id} onClick={() => this.addUserPledge()}>Make the pledge</buttton>}
+
                 <p className="top-info__title">{pledgeInfo[0].title}</p>
                 <p className="top-info__description">{pledgeInfo[0].description}</p>
                 <p className="top-info__number">{pledgeInfo[0].number_of_enrollement}</p>
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,10 +4,12 @@ const { userPledges } = require('./userPledges')
 const { getCategoryPledges } = require('./getCategoryPledges')
 const { getPledgePageInfo } = require('./getPledgePageInfo')
 const { confirmPledgeToUser } = require('./confirmPledgeToUser')
+const { deletePledgeToUser } = require('./deletePledgeToUser')
 
 router.get('/api/home/:userId', userPledges)
 router.get('/api/action-category/:categoryID', getCategoryPledges)
 router.get('/api/action-category/pledge/:pledgeID', getPledgePageInfo)
 router.post('/api/:userId/:pledgeId/addPledge', confirmPledgeToUser)
+router.get('/api/:userId/:pledgeId/deletePledge', deletePledgeToUser)
 
 module.exports = router
