699a141aab5597dd541dc012f7bd1cb5af26366c
Muniralsharif <munir.sniper@gmail.com>
2019-10-22 19:45:10 +0300

    commit to update master

--- client/src/components/ConfirmPage/index.js
+++ client/src/components/ConfirmPage/index.js
@@ -34,7 +34,7 @@ export class ConfirmPage extends Component {
   render() {
     return (
       <div className="confirm__Page">
-        <BackButton />
+        <BackButton {... this.props} />
 
         <p className="confirm__Page-letsConfirm">LET' S CONFIRM YOUR PLEDGE</p>
         <p className="confirm__Page-confirmPledge">
@@ -54,7 +54,7 @@ export class ConfirmPage extends Component {
           feel able to carry it out.
         </p>
 
-        <Footer />
+        <Footer {... this.props} />
       </div>
     );
   }
