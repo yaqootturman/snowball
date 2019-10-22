d2c76cd754c71ff115fba699b32f6015762f755b
Rand Inaim <rand.inaim95@gmail.com>
2019-10-22 12:00:11 +0300

    styling for delete confirmation page using flex

--- client/src/components/DeletePledgeConfirmation/index.js
+++ client/src/components/DeletePledgeConfirmation/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import Footer from '../Footer'
+import BackButton from '../BackButton'
 import './style.css'
 
 const DeletePledgeConfirmation = () => {
+  const pledgeName = 'flying'
   return (
-    <div className="delete-confirmation-dialog">
-      <h3 className="delete-confirmation-dialog__dialog-title" ><p className="delete-confirmation-dialog__title-message" >Delete Confirmation</p><icon className="delete-confirmation-dialog__delete-icon" >&#10005;</icon></h3>
-      <p className="delete-confirmation-dialog__delete-message">Are you Sure You Want To Delete The Pledge? </p>
-      <button className="delete-confirmation-dialog__confirm-button button-aligner">Yes, Delete</button>
-      <button className="delete-confirmation-dialog__cancel-button button-aligner">Cancel</button>
-    </div>
+    <>
+      <div className="delete-confirmation-dialog">
+        <div className="delete-page-back-button"><BackButton /></div>
+        <h3 className="delete-confirmation-dialog__dialog-title" >Delete Confirmation!</h3>
+        <p className="delete-confirmation-dialog__delete-message">Are you Sure You Want To Delete The Pledge {pledgeName} ?</p>
+        <img className="delete-confirmation-dialog__pledge-image" alt="pledge to delete photo" src="https://i.imgur.com/jC8Lx9b.png"></img>
+        <button className="delete-confirmation-dialog__confirm-button button-aligner">Yes, Delete</button>
+        <button className="delete-confirmation-dialog__cancel-button button-aligner">Cancel</button>
+      </div>
+      <Footer />
+    </>
   )
 }
 export default DeletePledgeConfirmation
