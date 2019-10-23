aa4b8f90af44f2a7484e94bfc361758c6fa31f0f
Rand Inaim <rand.inaim95@gmail.com>
2019-10-24 01:03:30 +0300

    modified the images coming to home page to be banner images

--- server/database/queries/getUserPledges.js
+++ server/database/queries/getUserPledges.js
@@ -2,5 +2,5 @@ const dbConnection = require('../db_connection')
 
 exports.getUserPledges = (userId) => {
   return dbConnection
-    .query('SELECT pledge.pledge_id,title,description,number_of_enrollement,img FROM pledge_to_user INNER JOIN pledge ON pledge_to_user.pledge_id = pledge.pledge_id INNER JOIN "user" ON pledge_to_user.user_id = "user".user_id WHERE pledge_to_user.user_id =$1', [userId])
+    .query('SELECT pledge.pledge_id,title,description,number_of_enrollement, pledge.banner_img FROM pledge_to_user INNER JOIN pledge ON pledge_to_user.pledge_id = pledge.pledge_id INNER JOIN "user" ON pledge_to_user.user_id = "user".user_id WHERE pledge_to_user.user_id =$1', [userId])
 }
