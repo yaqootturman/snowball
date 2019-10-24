6c009a4fbd6371fb1dec7e20687b71af2c6e55df
yaqootturman <yaqoot.turman@gmail.com>
2019-10-24 10:24:49 +0300

    frontend errors handle

---
+++ client/src/components/FrontEndError/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const FrontEndError = () => {
+  return (
+    <div className="error">
+      <h1>Not Found</h1>
+      <p>Enter correct path,Please!</p>
+    </div>
+  )
+}
+
+export default FrontEndError
