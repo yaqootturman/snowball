9dd8cd7d404d0a6ed2c9abd8d2ecf33db177091e
yaqootturman <yaqoot.turman@gmail.com>
2019-10-24 12:17:33 +0300

    server error handle in home page when something wrong with query

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -8,7 +8,8 @@ import './style.css'
 class HomePage extends Component {
   state = {
     userPledges: [],
-    loading: true
+    loading: true,
+    serverError: ""
   }
 
   componentDidMount() {
@@ -17,12 +18,12 @@ class HomePage extends Component {
       this.setState({ userPledges: data, loading: false })
     })
       .catch(error => {
-        console.log("error", error);
+        this.setState({ serverError: error.response.data.message })
       })
   }
 
   render() {
-    const { userPledges, loading } = this.state
+    const { userPledges, loading, serverError } = this.state
     const { history } = this.props
     const userMessage = 'You haven’t made any pledges yet!\n\n Head to your action dashboard to see how you can fight climate change today.'
 
@@ -30,16 +31,25 @@ class HomePage extends Component {
       <React.Fragment>
         <h1 className="home-title">My Pledges</h1>
         <h5 className="home-subtitle">TOTAL PLEDGES: {userPledges.length}</h5>
+        {serverError !== "" ? <h1>{serverError}</h1> : (
+          <>
+            {loading ? <h1>Loading</h1> : (!userPledges.length) ?
+              (<>
+                <p className="home-user-message">{nl2br(userMessage)}</p>
+                <button className="home-redirect-to-dashboard" onClick={() => history.push('/dashboard')}><img className="home-dashboard-icon" alt="dashboard icon" src="https://imgur.com/cWgJL1U.png" /><span className="dashboard-button">Action Dashboard</span></button>
+              </>) :
+              userPledges.map(onePledge => {
+                return <UserPledges {...this.props} userPledge={onePledge} key={onePledge.pledge_id} />
+              })
+            }
 
-        {loading ? <h1>Loading</h1> : (!userPledges.length) ?
-          (<>
-            <p className="home-user-message">{nl2br(userMessage)}</p>
-            <button className="home-redirect-to-dashboard" onClick={() => history.push('/dashboard')}><img className="home-dashboard-icon" alt="dashboard icon" src="https://imgur.com/cWgJL1U.png" /><span className="dashboard-button">Action Dashboard</span></button>
-          </>) :
-          userPledges.map(onePledge => {
-            return <UserPledges {...this.props} userPledge={onePledge} key={onePledge.pledge_id} />
-          })
+          </>
+        )
+
+
         }
+
+
         <Footer {...this.props} />
       </React.Fragment >
     )
--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const router = require('./controllers')
+const { serverError } = require('./middleware/error')
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
 app.use(express.json())
 app.use(express.urlencoded())
 
 app.use(router)
+app.use(serverError)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
