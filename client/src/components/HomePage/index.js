62af6e6f416574efa1405a517e523f7a7f153ddd
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-24 10:37:35 +0300

    make style  instead of loading word in home page

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -4,6 +4,7 @@ import UserPledges from '../UserPledges'
 import Footer from '../Footer'
 import axios from 'axios'
 import './style.css'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 class HomePage extends Component {
   state = {
@@ -31,14 +32,21 @@ class HomePage extends Component {
         <h1 className="home-title">My Pledges</h1>
         <h5 className="home-subtitle">TOTAL PLEDGES: {userPledges.length}</h5>
 
-        {loading ? <h1>Loading</h1> : (!userPledges.length) ?
-          (<>
-            <p className="home-user-message">{nl2br(userMessage)}</p>
-            <button className="home-redirect-to-dashboard" onClick={() => history.push('/dashboard')}><img className="home-dashboard-icon" alt="dashboard icon" src="https://imgur.com/cWgJL1U.png" /><span className="dashboard-button">Action Dashboard</span></button>
-          </>) :
-          userPledges.map(onePledge => {
-            return <UserPledges {...this.props} userPledge={onePledge} key={onePledge.pledge_id} />
-          })
+        {loading ? <div className="loading-spinner">
+          <ClipLoader
+            className="loading-spinner__home"
+            sizeUnit={'px'}
+            size={80}
+            color={'#123abc'}
+          />
+        </div> : (!userPledges.length) ?
+            (<>
+              <p className="home-user-message">{nl2br(userMessage)}</p>
+              <button className="home-redirect-to-dashboard" onClick={() => history.push('/dashboard')}><img className="home-dashboard-icon" alt="dashboard icon" src="https://imgur.com/cWgJL1U.png" /><span className="dashboard-button">Action Dashboard</span></button>
+            </>) :
+            userPledges.map(onePledge => {
+              return <UserPledges {...this.props} userPledge={onePledge} key={onePledge.pledge_id} />
+            })
         }
         <Footer {...this.props} />
       </React.Fragment >
