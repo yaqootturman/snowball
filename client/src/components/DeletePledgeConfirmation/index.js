71e9f159af73f7b4b26ada37ae79cfa6b1634f2f
Rand Inaim <rand.inaim95@gmail.com>
2019-10-27 14:27:12 +0200

    update delete confirmation page design

--- client/src/components/DeletePledgeConfirmation/index.js
+++ client/src/components/DeletePledgeConfirmation/index.js
@@ -6,16 +6,8 @@ import './style.css'
 
 const DeletePledgeConfirmation = (props) => {
 
-
-
-
   const { pledgeId } = props.match.params
-  //get pledge info and handleChange function of the pledge to change the pledgeExit state in the pledgePage
-  let { pledge_name, pledge_image } = props.location
   let { history } = props
-
-  //  user_id will be changed to be taken from authentication cookies
-  //  handleChange function is called to change the state of pledge
   const deleteUserPledge = () => {
     const userId = 1;
     axios.get(`/api/${userId}/${pledgeId}/deletePledge`).then(
@@ -25,22 +17,13 @@ const DeletePledgeConfirmation = (props) => {
     ).catch(err => { console.log("axios err", err) })
   }
 
-  //read pledge info from session storage in case the page is refreshed and the props are lost
-  if (!pledge_name) {
-    const sessionData = sessionStorage.getItem('pledgeInfo');
-    ({ pledge_name, pledge_image } = JSON.parse(sessionData));
-  }
-
   return (
 
     <>
       <div className="delete-confirmation-dialog">
         <div className="delete-confirmation-dialog__back-button"><BackButton {...props} /></div>
-        <h3 className="delete-confirmation-dialog__dialog-title" >Delete Confirmation!</h3>
-        <p className="delete-confirmation-dialog__delete-message">Are you Sure You Want To Delete {pledge_name} Pledge ?</p>
-        <img className="delete-confirmation-dialog__pledge-image" alt="pledge to delete " src={pledge_image}></img>
-        <button className="delete-confirmation-dialog__confirm-button button-aligner" onClick={() => deleteUserPledge()}>Yes, Delete</button>
-        <button className="delete-confirmation-dialog__cancel-button button-aligner" onClick={() => history.goBack()}>Cancel</button>
+        <p className="delete-confirmation-dialog__delete-message">Are you Sure You Want To Delete This Pledge ?</p>
+        <button className="delete-confirmation-dialog__confirm-button button-aligner" onClick={() => deleteUserPledge()}>Yes, please remove the pledge</button>
       </div>
       <Footer {...props} />
     </>
