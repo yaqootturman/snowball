393c946c5261f4a884ba99608139c8ec21b88b6c
yaqootturman <yaqoot.turman@gmail.com>
2019-10-28 10:56:13 +0200

    travis

--- client/src/components/Category/index.js
+++ client/src/components/Category/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import './style.css'
-import { Switch } from "react-router-dom";
-import ClipLoader from 'react-spinners/ClipLoader'
+
 
 
 class Category extends Component {
