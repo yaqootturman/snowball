d48c5e0ca6046b1f360ae5402da541636a46fb0b
DuaaH <dodohalaiqa.1@gmail.com>
2019-10-28 11:46:53 +0200

    edit style

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -29,33 +29,35 @@ class HomePage extends Component {
     const userMessage = 'You haven’t made any pledges yet!\n\n Head to your action dashboard to see how you can fight climate change today.'
 
     return (
-      <div className="home">
-        <h1 className="home__title">My Pledges</h1>
-        <h5 className="home__subtitle">TOTAL PLEDGES: {userPledges.length}</h5>
-        {serverError !== "" ? <h1>{serverError}</h1> : (
-          <>
-            {loading ? <div className="loading-spinner">
-              <ClipLoader
-                className="loading-spinner__home"
-                sizeUnit={'px'}
-                size={80}
-                color={'#123abc'}
-              />
-            </div> : (!userPledges.length) ?
+      <>
+        <div className="home">
+          <h1 className="home__title">My Pledges</h1>
+          <h5 className="home__subtitle">TOTAL PLEDGES: {userPledges.length}</h5>
+
+          {serverError !== "" ? <h1>{serverError}</h1> : (
+
+            loading ?
+              (<div className="loading-spinner">
+                <ClipLoader
+                  className="loading-spinner__home"
+                  sizeUnit={'px'}
+                  size={80}
+                  color={'#123abc'}
+                />
+              </div>) :
+              (!userPledges.length) ?
                 (<>
                   <p className="home__user-message">{nl2br(userMessage)}</p>
                   <button className="home__redirect-to-dashboard" onClick={() => history.push('/dashboard')}><img className="home__dashboard-icon" alt="dashboard icon" src="https://imgur.com/cWgJL1U.png" /><span className="home__dashboard-button">Action Dashboard</span></button>
                 </>) :
                 userPledges.map(onePledge => {
                   return <UserPledges {...this.props} userPledge={onePledge} key={onePledge.pledge_id} />
-                })
-            }
-            <Footer {...this.props} />
-          </>
-        )}
+                }))}
 
 
-      </div >
+        </div >
+        <Footer {...this.props} />
+      </>
     )
 
   }
