35dea0066eb1b71f927533a35c9ada3d507651fa
yaqootturman <yaqoot.turman@gmail.com>
2019-10-30 16:07:17 +0200

    click img

--- client/src/components/UserPledges/index.js
+++ client/src/components/UserPledges/index.js
@@ -3,8 +3,8 @@ import './style.css'
 
 const UserPledge = ({ history, userPledge: { pledge_id, title, description, number_of_enrollement, banner_img } }) => {
   return (
-    <div className="user-pledge">
-      <img className="user-pledge__pledge-image" alt="user pledge" src={banner_img} onClick={() => { history.push(`/action-category/pledge/${title}`, { pledge_id: pledge_id }) }} />
+    <div className="user-pledge" onClick={() => { history.push(`/action-category/pledge/${title}`, { pledge_id: pledge_id }) }}>
+      <img className="user-pledge__pledge-image" alt="user pledge" src={banner_img}  />
       <h5 className="user-pledge__pledge-title">{title}</h5>
       <span className="user-pledge__pledge-description">{description}</span>
       <div className="user-pledge__pledge-enrollements-badge"><img className="user-pledge__pledge-enrollements-icon" alt="people" src="https://imgur.com/qw15p7I.png" />
